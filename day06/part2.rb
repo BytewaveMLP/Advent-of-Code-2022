5399f046f80778de8b5f254e363bda8447d5c5d7
Eliot Partridge <byte@code.horse>
2022-12-05 23:07:27 -0600

    Day 06

---
+++ day06/part2.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative '../lib/prelude'
+
+WINDOW_SIZE = 14
+
+input = $input.first
+
+(0...input.size - WINDOW_SIZE).each do |i|
+  next unless Set.new(input[i...i + WINDOW_SIZE].chars).size == WINDOW_SIZE
+
+  puts(i + WINDOW_SIZE)
+  puts(input[i...i + WINDOW_SIZE])
+end
